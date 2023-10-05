9a4c3bf75b0632d013ba40faa5faf0588a856625
ayesharajput <143730428+ayesharajput@users.noreply.github.com>
2023-10-05 20:51:33 +0500

    Add files via upload

---
+++ java class06/app.js
@@ -0,0 +1,227 @@
+
+
+//   function name(a,b){
+//     console.log(a+b);
+// }
+// var a=prompt("Enter your firstName");
+//   var b=prompt("Enter your lastName ");
+//  name(a,b);
+
+
+//chpter 36 passing an argument in function
+//   var num=+prompt("Enter number ");
+//   var c =+prompt("eNTER YOUR LENGHT");
+// function table(a,d){
+// for(var i=1; i<=c;i++){
+//     console.log(num + "X" + i + "="+ num*i);
+// }
+// }
+// table(num,c);
+
+
+//chp 37 passing databack from them 
+
+// function sum (num1,num2){
+//     return num1+num2;
+// }
+// var num1= +prompt("enter first num")
+// var num2= +prompt("Enter second num");
+// var result=sum(num1,num2);
+// console.log(result);
+
+
+
+
+// function calc(num,opr,num2){
+            
+
+// if (opr==="+"){
+//     return num1+num2;
+// }
+//  else if (opr==="-"){
+//     return num1-num2;
+// }
+//  else if (opr==="*"){
+//     return num1*num2;
+// }
+// else if (opr==="/"){
+//     return num1/num2;
+// }
+// }
+
+// var num1=+prompt("Enter Number");
+// var opr = prompt("Enter an opretor","+ , - , *, /");
+// var num2=+prompt("Enter Number");
+
+// var result=calc(num1,opr,num2);
+// console.log(result);
+
+
+//chp 39 switchg statements
+
+
+
+// var a =prompt("enter a day");
+//  var arr= ["mon","tue","wed","thurs","fri"];
+ 
+//  switch(a){
+// case arr[0]:
+//  alert("its monday!");
+// break;
+ 
+//  case arr[1]:
+//  alert("its tuesday!");
+// break;
+ 
+// case arr[2]:
+//  alert("its wednesday!");
+// break;
+
+// case arr[3]:
+//  alert("its thursday!");
+// break;
+
+// case  arr[4]:
+//  alert("its fri!");
+// break;
+
+// default:
+//     alert("its weekend");
+// }
+
+// var table = +prompt("enter table ")
+// var tableLength = +prompt("enter table length")
+
+// var i = 0 ;
+// while (i <= tableLength){
+//     console.log(table+"X"+ i+"="+table*i);
+//     i++
+// }
+
+
+
+// var table2 = +prompt("enter table ")
+// var tableLength2 = +prompt("enter table length")
+
+// var i = 0 ;
+// do{
+//     console.log(table2+"X"+ i+"="+table2*i);
+//     i++
+// }
+// while (i <= tableLength2)
+
+
+
+// var day =prompt("enter a day");
+ 
+// switch(day)
+
+// {
+
+//     case "monday":
+//         alert("its monday")
+//         break;
+    
+//         case "tuesday":
+//             alert("its tuesday")
+//             break;
+    
+//             case "wednesday":
+//                 alert("its wednesday")
+//                 break;
+                
+    
+//                 case "thrusday":
+//                     alert("its thrusday")
+//                     break;
+                
+//                     case "friday":
+//                         alert("its friday")
+//                         break;
+                
+//                         case "saturday":
+//                             alert("its saturday")
+//                             break;            
+                
+//                             case "sunday":
+//                                 alert("its sunady")
+//                                 break;    
+//                 default:
+//                     alert("its not a day ")
+// }
+
+
+const cars = ["BMW", "Volvo", "Saab", "Ford"];
+var i = 0
+while (i<=9){
+    console.log (cars[i])
+}
+
+var word = prompt("enter word");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
